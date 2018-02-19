9cdd5738a004b0c7f945be1b6d43dfdc4c8c58e9
jaredryan <jryantennis@berkeley.edu>
2018-02-19 00:06:00 +0000

    [feat] search by lake works; posts and puts, as well as search by zip, ready but not connected to API

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,18 +11,41 @@ class MainController < ApplicationController
     end
     
     def search
+        session.delete(:results)
         @water = params[:water]
         @zip = params[:zip]
+        
+        if @water === "" && @zip === ""
+            flash[:notice] = "Please enter a lake name or a zip code."
+            redirect_to root_path
+            return
+        elsif @zip != ""
+            flash[:notice] = "Searching by zip code is not yet functional."
+            redirect_to root_path
+            return
+        end
+        
         flash[:searched] = true
         
         # Write code to update page but not refresh with filtered results
-        endpoint = "http://www.freshwaterapi.com/api/freshwater/getlakesbyzip/"
-        # @response = HTTParty.get(endpoint + @zip)
+        endpoint = "http://freshwater.dynu.net/api/freshwater/"
         
-        # If valid, display responses if valid
-        # Else, flash[:notice] = "Your input did not find a match in the database."
+        @water_response = HTTParty.get(endpoint + "SearchLake/" + @water)
+        # @zip_info = 
+        @water_info = @water_response.parsed_response[0]
         
-        # Below redirect currently refreshes the page
+        @name = @water_info["LakeName"]
+        @general = @water_info["Generals"]
+        @username = @water_info["AppUser"] || "Unknown User"
+        @zip_code = @water_info["ZipCode"]["Zip"] || "Unknown Zip Code"
+        
+        session[:results] = [{name: @name, general: @general, username: @username, zip_code: @zip_code}]
+
+        # Currently not functional
+        # @zip_response = HTTParty.get(endpoint + "GetLakeByZip/" + @zip)
+
+        
+
         redirect_to root_path
     end
 
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,6 +8,10 @@ class ReportsController < ApplicationController
     end
     
     def new
+        if not logged_in?
+            flash[:notice] = "You must login to submit a report."
+            redirect_to login_path
+        end
     end
     
     def open
@@ -25,25 +29,85 @@ class ReportsController < ApplicationController
     end
 
     def create
+        @zip = params[:zipPost]
+        @lake = params[:waterPost]
+        
+        if @lake === ""
+            flash[:notice] = "Please enter a lake name."
+            redirect_to root_path
+            return
+        end
         
         @boat_traffic = params[:boatTraffic]
         @beach_crowd = params[:beachCrowd]
         @boat_ramp = params[:boatRamp]
         @general = params[:text]
         
-        flash[:clicked] = nil
-        # Put data into database
+        @generalForm = {
+            boatTraffic: @boat_traffic,
+            beachCrowd: @beach_crowd,
+            boatRamp: @boat_ramp,
+            text: @general
+        }
         
+        # Check to see if it already exists
+        endpoint = "http://freshwater.dynu.net/api/freshwater/"
+        @water_response = HTTParty.get(endpoint + "SearchLake/" + @lake)
+        @water_info = @water_response.parsed_response[0]
+        
+        # Use put or post as needed
+        # if @water_info === nil || @water_info["ZipCode"]["Zip"] != @zip
+        #     # Add way to get more location information based off of zip code
+        #         ### AWESOME ZIP CODE SEARCH API SEARCH THING CODE
+        #         ### Use this to fill out zipcode sub-object
+        #     # Post if it doesn't exist
+        #     @response = HTTParty.post(endpoint, body: {
+        #         "LakeName": @lake,
+        #         "LakeDeleted": false,
+        #         "AppUser": null,
+        #         "Biologicals": [],
+        #         "Environmentals": [],
+        #         "Generals": [@generalForm],
+        #         "ZipCode": {
+        #             "ZipCodeID": 51,
+        #             "Zip": @zip,
+        #             "City": "",
+        #             "State": "",
+        #             "Latitude": "",
+        #             "Longitude": ""
+        #         },
+        #         "Socials": []
+        #         })
+        # else
+        #     # Put if it does exist
+            
+        #     @generalArray = @water_info["Generals"]
+        #     @generalArray.unshift(@generalForm)
+        #     @response = HTTParty.put(endpoint + "SearchLake/" + @lake, body: {
+        #         "Generals": @generalArray
+        #     })
+        # end
+        
+        
+        flash[:clicked] = nil
         flash[:notice] = "Thank you for your submission!"
-    
-        # @report = Report.create(params)
+
         redirect_to root_path
     end
     
     def env_create
         
-       # Good
+        @zip = params[:zip]
+        @lake = params[:water]
         
+        if @zip === "" && @lake === ""
+            flash[:notice] = "Please enter a lake name or a zip code."
+            redirect_to new_report_path
+            return
+        end
+        
+        # Good
+       
         # @boat_traffic = params[:boatTraffic]
         # @beach_crowd = params[:beachCrowd]
         # @boat_ramp = params[:boatRamp]
@@ -53,12 +117,23 @@ class ReportsController < ApplicationController
         # Put data into database
         
     
+        # byebug
+        
         # @report = Report.create(params)
         flash[:notice] = "Thank you for your submission!"
         redirect_to root_path
     end
     
     def bio_create
+        
+        @zip = params[:zip]
+        @lake = params[:water]
+        
+        if @zip === "" && @lake === ""
+            flash[:notice] = "Please enter a lake name or a zip code."
+            redirect_to new_report_path
+            return
+        end
         
         # Good
         
@@ -77,6 +152,15 @@ class ReportsController < ApplicationController
     end
     
     def soc_create
+        
+        @zip = params[:zip]
+        @lake = params[:water]
+        
+        if @zip === "" && @lake === ""
+            flash[:notice] = "Please enter a lake name or a zip code."
+            redirect_to new_report_path
+            return
+        end
         
         # Good
         
